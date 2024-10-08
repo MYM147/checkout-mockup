75450e2c57097c0bd1a864122d6ef7584f19f5ab
Michael Mastrucci <michael.mastrucci@sherwin.com>
2024-10-08 10:40:11 -0400

    Conditional rendering for empty cart

--- src/components/Cart/ViewCartPage.jsx
+++ src/components/Cart/ViewCartPage.jsx
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import Sidebar from '../Sidebar';
+import EmptyCart from './EmptyCart';
 import ShoppingCart from './ShoppingCart';
 
 const ViewCartPage = () => {
+	const [cartIsEmpty, setCartIsEmpty] = useState(false)
+
   return (
-			<div className='md:swdc-flex swdc-max-w-[995px]'>
-				<ShoppingCart />
-				<Sidebar />
-			</div>
+		<div className='md:swdc-flex swdc-max-w-[995px]'>
+			{cartIsEmpty ? (
+				<EmptyCart />
+			) : (
+				<>
+					<ShoppingCart />
+					<Sidebar />
+				</>
+			)}
+		</div>
 	);
 }
 
