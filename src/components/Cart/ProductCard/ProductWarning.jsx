d6288b9856eec5a32be55c3e1432a6a35a892594
Michael Mastrucci <michael.mastrucci@sherwin.com>
2024-10-10 13:54:05 -0400

    revert

--- src/components/Cart/ProductCard/ProductWarning.jsx
+++ src/components/Cart/ProductCard/ProductWarning.jsx
@@ -22,14 +22,15 @@ const ProductWarning = () => {
   return (
 		<>
 			<div className='md:swdc-ml-[15%] sm:swdc-ml-2'>
-				<div className='swdc-text-xs swdc-flex swdc-flex-col'>
+				<div className='swdc-text-xs sm:swdc-flex-col'>
 					<div className='swdc-flex md:swdc-order-1 swdc-order-2'>
 						<div className='swdc-pr-2 swdc-w-[270px]'>
 							<ul className='swdc-flex swdc-justify-evenly'>
 								<li className='swdc-ml-1 hover:swdc-underline'>
 									<Button
 										variant='text'
-										className='swdc-text-[#0069ae] swdc-text-[0.95em] hover:swdc-bg-white swdc-normal-case swdc-tracking-widest'>Edit
+										className='swdc-text-[#0069ae] swdc-text-[0.95em] hover:swdc-bg-white swdc-normal-case swdc-tracking-widest'>
+											Edit
 									</Button>
 								</li>
 								<li className='swdc-border swdc-h-2 swdc-my-auto swdc-mx-[-5px]'></li>
@@ -54,7 +55,7 @@ const ProductWarning = () => {
 						</div>
 					</div>
 
-					<div className='swdc-flex swdc-w-full md:swdc-w-[73%] md:swdc-relative md:swdc-left-1 md:swdc-mb-2 md:swdc-mt-0 swdc-mt-3 swdc-px-2 swdc-order-1'>
+					<div className='swdc-flex swdc-w-[73%] md:swdc-relative md:swdc-left-1 swdc-mb-2 swdc-px-2 swdc-order-1'>
 						<div className='swdc-relative swdc-mr-1 swdc-align-middle'>
 							<IconFillWarning className='swdc-icon-2 swdc-fill-[#fefe04]' />
 							<div className='swdc-absolute swdc-top-0 swdc-left-0'>
