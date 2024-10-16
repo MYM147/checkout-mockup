2d0a6d8cda47737a8ebec716121faa2dc9ab320f
Michael Mastrucci <michael.mastrucci@sherwin.com>
2024-10-16 14:40:28 -0400

    Cleanup

--- src/components/Cart/ViewCart.jsx
+++ src/components/Cart/ViewCart.jsx
@@ -1,4 +1,4 @@
-import { IconRegularDelete } from '@prism/dropcloth';
+// import { IconRegularDelete } from '@prism/dropcloth';
 import { useState } from 'react';
 import ProductButtons from '../Buttons/ProductButtons';
 import Sidebar from '../Sidebar';
@@ -21,9 +21,9 @@ const ViewCart = () => {
 		console.log('View Cart ', newProduct);
 	};
 
-	const handleDelete = () => {
-		setCartItems([]);
-	};
+	// const handleDelete = () => {
+	// 	setCartItems([]);
+	// };
 
 	const updateQuantity = (productId, newQuantity) => {
 		setCartItems((prevItems) =>
@@ -43,14 +43,14 @@ const ViewCart = () => {
 		<div className='swdc-bg-[#fff] swdc-max-w-[995px] swdc-mx-auto swdc-pb-6'>
 			<div className='swdc-flex swdc-justify-between swdc-w-full swdc-mx-auto swdc-align-middle swdc-mb-4 swdc-pb-2 swdc-pt-2 swdc-px-6 swdc-border-b swdc-border-[#e5e5e5]'>
 				<h1 className='swdc-text-[28px] swdc-align-middle'>Cart</h1>
-				<div className='swdc-w-[60px] swdc-text-center '>
+				{/* <div className='swdc-w-[60px] swdc-text-center '>
 					<IconRegularDelete className='swdc-icon-1 swdc-fill-[#318bd3] swdc-mx-auto' />
 					<a
 						className='swdc-uppercase swdc-text-[10px] swdc-text-[#318bd3] swdc-text-center swdc-font-bold'
 						onClick={handleDelete}>
 						Delete
 					</a>
-				</div>
+				</div> */}
 			</div>
 			<ProductButtons onProductSelect={handleProductSelect} />
 			{cartItems.length === 0 ? (
